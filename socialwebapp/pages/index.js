8dc3dec48db7d41136444174f6e755e790eb4f93
Anees Imran <aneesimran@outlook.com>
2021-05-19 16:30:45 +0100

    init

---
+++ socialwebapp/pages/index.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Social Media</title>
+        <meta name="description" content="Created in Next.JS" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Welcome</h1>
+
+      {/* Header */}
+
+      <main>
+        {/* sidebar */}
+        {/* feed */}
+        {/* widgets */}
+      </main>
+    </div>
+  );
+}
