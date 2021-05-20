44181340143a0370c93254c7afc9cfe01a708e6b
Anees Imran <aneesimran@outlook.com>
2021-05-20 13:01:48 +0100

    building middle section of the header

--- socialwebapp/components/Header.js
+++ socialwebapp/components/Header.js
@@ -2,24 +2,29 @@ import React from "react";
 import Image from "next/image";
 import {
   BellIcon,
-  ChatIcon,
   ChevronDownIcon,
   HomeIcon,
   UserGroupIcon,
   ViewGridIcon,
 } from "@heroicons/react/solid";
-import { FlagIcon, PlayIcon, SearchIcon } from "@heroicons/react/outline";
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ChatAlt2Icon,
+} from "@heroicons/react/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
+    <div className="sticky top- z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
       {/* Left */}
       <div className="flex items-center">
-        <Image src="/connection.png" width={40} height={40} layout="fixed" />
+        <Image src="/network.png" width={40} height={40} layout="fixed" />
         <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
           <SearchIcon className="h-6 text-gray-600" />
           <input
-            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 "
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink "
             type="text"
             placeholder="Search"
           />
@@ -27,6 +32,14 @@ function Header() {
       </div>
 
       {/* Center */}
+
+      <div className="flex justify-center flex-grow ">
+        <div className="flex space-x-6 md:space-x-10">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={ChatAlt2Icon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
       {/* Right */}
     </div>
--- socialwebapp/tailwind.config.js
+++ socialwebapp/tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      borderColor: ["active"],
+      borderWidth: ["active"],
+    },
   },
   plugins: [],
 };
