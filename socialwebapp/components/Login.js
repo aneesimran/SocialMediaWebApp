c0b909b4308ebe44667c3906fb5d45bbb936a3a9
Anees Imran <aneesimran@outlook.com>
2021-05-20 15:54:11 +0100

    added google sign in support

--- socialwebapp/components/Login.js
+++ socialwebapp/components/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import { signIn } from "next-auth/client";
 
 function Login() {
-  return <div></div>;
+  return (
+    <div className="grid place-items-center mt-60">
+      <Image src="/network.png" height={300} width={300} objectFit="contain" />
+      <h1
+        onClick={signIn}
+        className="p-5 bg-purple-500 rounded-full text-white 
+      text-center mt-10 cursor-pointer"
+      >
+        Login with Google
+      </h1>
+    </div>
+  );
 }
 
 export default Login;
