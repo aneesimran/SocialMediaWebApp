82714f2f995ced2f6f08b740ddcce8d3f94c6ede
Anees Imran <aneesimran@outlook.com>
2021-06-08 15:41:42 +0100

    Update [...nextauth].js

--- socialwebapp/pages/api/auth/[...nextauth].js
+++ socialwebapp/pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientId: 680895175751-6t72ofpof6b7ddn463jkjrt4hi2dfttr.apps.googleusercontent.com,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
